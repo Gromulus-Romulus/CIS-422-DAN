7f80e56e174924a8921c8ddd85cfef30e75ecbfb
Elizabeth Bailey <elizachizbailey@gmail.com>
2022-05-30 20:28:05 -0700

    quiz generator working

--- DANv1/website/views.py
+++ DANv1/website/views.py
@@ -10,6 +10,20 @@ def home():
 def login():
     return render_template("login.html")
 
+@views.route('/signup')
+def start_quiz():
+    q = get_questions()
+    return render_template("signup.html", questions=q)
+
+def get_questions():
+    questions = [['question a', ['answer a1', "answer a2"]],['question b', ['answer b1', 'answer b2']],['question c', ['answer 1c', 'answer 2c']]]
+    # TODO get actual data for here
+    return questions
+
+@views.route('/for-shelters')
+def shelter():
+    return render_template("shelter-login.html")
+
 @views.route('/about')
 def about():
     return render_template("about.html")
@@ -17,3 +31,11 @@ def about():
 @views.route('/dogs')
 def dogs():
     return render_template("dogs.html")
+
+@views.route('/test')
+def test():
+    q = get_questions()
+    return render_template("test.html", questions=q)
+
+
+# TODO use add_url_rule instead? may have to restructure files to access "app"
