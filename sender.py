778c4a648834ebce0f74758f9e22b6adb110fd8c
Nathan Malamud <58883495+Gromulus-Romulus@users.noreply.github.com>
2022-06-06 13:34:45 -0700

    Sending dog profiles

---
+++ sender.py
@@ -0,0 +1,32 @@
+# Sends randomly-generated dog profiles to database.
+#
+# Author: Nathan M.
+#
+
+from DANv1.dogdb import add_dog, delete_dog, get_dogs, get_dog_by_ID
+
+# import dog profiles from csv table
+
+with open("./DAN_dog_profiles.csv") as PROFILES:
+    header = PROFILES.readline()
+
+    for line in PROFILES:
+
+        # Split by comma and get rid of ID
+        fields = line.split(',')
+
+        [
+            name, photo_id, sex, age, breed, grouping, weight, notes,
+            neutered, walks, barking, trained,
+            yard_req, friendly, energy,
+            attention_req, good_with_pets,
+            good_with_kids, shedding, size
+        ] = fields
+
+        # add dog to database
+        add_dog(name, photo_id, sex, age, breed, grouping,
+               weight, notes, neutered,
+               walks, barking, trained,
+               yard_req, friendly, energy,
+               attention_req, good_with_pets,
+               good_with_kids, shedding, size)
