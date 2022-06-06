534ab821d207437453146085fee4814dcf747bb4
Elizabeth Bailey <elizachizbailey@gmail.com>
2022-06-06 13:49:37 -0700

    dog profile page updates

--- DANv1/website/dog.py
+++ DANv1/website/dog.py
@@ -4,7 +4,11 @@
 # this class is only used to display an individual dog's profile
 # it's structured like this to make it easier to manage changes to the database structure 
 class Dog:
-    def __init__(self, ID, name, age, weight, sex, breed, notes, neuspay, walks, barking, trained, training_time, yard_requirement, friendly, energy, attention_requirement, good_with_pets, good_with_kids, shedding, size, photo, group):
+    def __init__(self, ID, name, photo, sex, age, breed, grouping, weight, notes, 
+fixed, walks, barking, trained, 
+training_time, yard_req, friendly, 
+energy, attention_req, good_with_pets,
+good_with_kids, shedding, size):
         self.id = ID
         # maybe just generate size from weight using a range
         self.name = name
@@ -25,9 +29,21 @@ class Dog:
             self.size = ""
         self.age = age
         self.breed = breed
+        self.group = grouping
         self.weight = weight
         self.bio = notes
-        self.neuspay = neuspay
+        self.neuspay = fixed
+        self.walks = walks
+        self.bark = barking
+        self.trained = trained
+        self.yard = yard_req
+        self.friendly = friendly
+        self.energy = energy
+        self.kids = good_with_kids
+        self.pets = good_with_pets
+        self.shedding = shedding
+        self.img = photo
+        self.attention = attention_req
 
 
 # def render_dog(dbTuple)
